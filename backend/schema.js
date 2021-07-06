2a5518deddcaaadff8f5d9110d9581c537a13d42
Cengizhan Ziyaeddin <cengiz.ziyaeddin@sigmalabs.xyz>
2021-07-06 16:03:25 +0100

    schema.js completed

--- backend/schema.js
+++ backend/schema.js
@@ -1,34 +1,28 @@
 
 import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts"
-import { config } from 'https://deno.land/x/dotenv/mod.ts'
+import { config } from "https://deno.land/x/dotenv/mod.ts";
 
 //Get deno environment 
 const DENO_ENV = Deno.env.get('DENO_ENV') ?? 'development'
-
-//configure path to environment file.
 config({ path: `./.env.${DENO_ENV}`, export: true })
 
-//!Temporary hardocoded url. 
-// const PG_URL = 'postgres://xwwfuwxv:XeEM9GGiu8_guphTKafAevWvgtF5u8IY@rogue.db.elephantsql.com/xwwfuwxv'
-
-const client = new Client(Deno.env.get('PG_URL'))
-
+const client = new Client(Deno.env.get("PG_URL"))
 await client.connect()
 
 await client.queryObject(
-    `CREATE TABLE recipes (
-        id SERIAL PRIMARY KEY,
-        name TEXT UNIQUE NOT NULL,
-        instructions TEXT UNIQUE NOT NULL,
-        ingredients TEXT NOT NULL,
-        tags TEXT NOT NULL,
-        created_at TIMESTAMP NOT NULL,
-        updated_at TIMESTAMP NOT NULL
+`CREATE TABLE IF NOT EXISTS recipes (
+    id SERIAL PRIMARY KEY,
+    name TEXT UNIQUE NOT NULL,
+    instructions TEXT UNIQUE NOT NULL,
+    ingredients TEXT NOT NULL,
+    tags TEXT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL
     );`
 )
 
 await client.queryObject(`
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
     id SERIAL PRIMARY KEY,
     username TEXT UNIQUE NOT NULL,
     email TEXT UNIQUE NOT NULL,
@@ -40,8 +34,8 @@ CREATE TABLE users (
 )
 
 await client.queryObject(`
-    CREATE TABLE sessions (
-        uuid TEXT PRIMARY KEY
+    CREATE TABLE IF NOT EXISTS sessions (
+        uuid TEXT PRIMARY KEY,
         created_at TIMESTAMP NOT NULL,
         expiry_date TIMESTAMP NOT NULL,
         user_id INTEGER,
@@ -50,7 +44,7 @@ await client.queryObject(`
 )
 
 await client.queryObject(`
-    CREATE TABLE rating (
+    CREATE TABLE IF NOT EXISTS rating (
         id SERIAL PRIMARY KEY,
         rating INT NOT NULL,
         CHECK (rating BETWEEN 1 AND 5),
@@ -63,7 +57,7 @@ await client.queryObject(`
 
 
 await client.queryObject(`
-    CREATE TABLE saved_recipes (
+    CREATE TABLE IF NOT EXISTS saved_recipes (
         id SERIAL PRIMARY KEY,
         created_at TIMESTAMP NOT NULL,
         updated_at TIMESTAMP NOT NULL,
@@ -74,19 +68,18 @@ await client.queryObject(`
 )
 
 await client.queryObject(`
-    CREATE TABLE comments (
+    CREATE TABLE IF NOT EXISTS comments (
         id SERIAL PRIMARY KEY,
         created_at TIMESTAMP NOT NULL,
         updated_at TIMESTAMP NOT NULL,
         comment TEXT NOT NULL,
         recipe_id INTEGER,
-        FORIEGN KEY(recipe_id) REFERENCES recipes(id)
+        FOREIGN KEY(recipe_id) REFERENCES recipes(id)
     );`
 )
 
-
 await client.queryObject(`
-        CREATE TABLE comment_votes (
+        CREATE TABLE IF NOT EXISTS comment_votes (
             id SERIAL PRIMARY KEY,
             direction TEXT NOT NULL DEFAULT 'up',
             created_at TIMESTAMP NOT NULL,
