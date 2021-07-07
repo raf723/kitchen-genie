e2ad174717158d146205fcf4d5c30a7254bc7b5f
Keano Robinson <keano@Keanos-MBP.home>
2021-07-07 11:08:22 +0100

    Add initial implementation of /register endpoint

---
+++ backend/function-assets/verify.js
@@ -0,0 +1,32 @@
+/* A Library of boilerplate methods for forms with certain types of components */
+
+import { includesNumber } from "./helpers"
+
+
+export function isEmailValid(email) {
+    const emailPattern = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)+$/
+    return email && emailPattern.test(email)
+}
+
+export function isPasswordValid(password) {
+  return (
+    password
+    && password.length >= 8  
+    && password.match(/[a-z]/) 
+    && password.match(/[A-Z]/) 
+    && includesNumber(password) 
+    && password.match(/[^A-Za-z0-9]/) 
+  )
+}
+
+export function isUsernameValid(proposedName) {
+  return (
+    proposedName
+    && proposedName.length !== 0
+    && proposedName.length <= 50
+  )
+}
+
+export const verify = { isEmailValid, isPasswordValid, isUsernameValid }
+
+export default verify
--- backend/recipeBackend.js
+++ backend/recipeBackend.js
@@ -11,6 +11,7 @@ import { v4 } from 'https://deno.land/std/uuid/mod.ts'
 // Database imports
 import { Client } from "https://deno.land/x/postgres@v0.11.3/mod.ts"
 import { config } from 'https://deno.land/x/dotenv/mod.ts'
+import verify from './function-assets/verify'
 
 // Envirionment setup
 const DENO_ENV = Deno.env.get('DENO_ENV') ?? 'development'
@@ -33,6 +34,54 @@ const headersWhiteList = [
 // Global variables
 let authenticated = false
 let userID = ''
+
+//Some useful functions
+
+//Authentication functions
+const universalSalt = "$2a$08$a1eVke4/bxYSkIJhBT6rcu"
+async function encryptPassword(password, salt="") {
+  if (!salt) { salt = await bcrypt.genSalt(8)}
+  password = await bcrypt.hash(password + universalSalt + salt)
+  return { password, salt }
+}
+
+async function isRegisteredUser(email, password) {
+  if (email && password) {
+    const userWithEmail = await getUser({ email })
+    if (userWithEmail) {
+      return await bcrypt.compare(password + universalSalt + userWithEmail.salt,
+          userWithEmail.encrypted_password)
+      }
+  } else {
+      return false
+  }
+} 
+
+//Query functions
+//Function to get a user from the users table in database
+async function getUser({email: undefined, userID: undefined, username: undefined}) {
+  if (userID !== undefined) {
+    return (await client.queryObject(`SELECT * FROM users WHERE id = $1;`, userID)).rows[0] || null
+  } else if (email !== undefined) {
+    return (await client.queryObject(`SELECT * FROM users WHERE email = $1;`, email)).rows[0] || null
+  } else if (username !== undefined) {
+    return (await client.queryObject(`SELECT * FROM users WHERE name = $1;`, username)).rows[0] || null
+  } else {
+    return null 
+  }
+}
+
+//Function to add a user to the users table of the database
+export async function addUser(email, password, name) {
+  try {
+    const encryption = await encryptPassword(password)
+    await client.queryObject(`INSERT INTO users (username, email, encrypted_password, salt, created_at, updated_at)
+      VALUES ($1, $2, $3, $4, datetime('now'), datetime('now'));`, username, email, encryption.password, encryption.salt)
+      return true
+  } catch(error) {
+    return false 
+  }
+}
 
 app
   .use(cors({ allowHeaders: headersWhiteList, allowCredentials: true, allowOrigins: [Deno.env.get("ALLOWED_ORIGINS")] }))
@@ -102,6 +151,25 @@ app
   })
 
 
+  //------------------------- Registration Handler -------------------------//
+  .post('/register', async (server) => {
+    let { email, password, username } = await server.body
+
+    if(verify.isEmailValid(email) && verify.isPasswordValid(password) && verify.isUsernameValid(username, true)) {
+        username = username.trim()
+        if (await getUser({ email })) {
+            //Error:Already a user
+            server.json({ response:`already registered` })
+        } else {
+            //All good
+            await addUser(email, password, username)
+            server.json({ response:`success`})
+        }
+    } else {
+        //Error:Invalid email, username or password
+        server.json({  response: `bad credentials` })
+    }
+  })
 
   //------------------------- Start server -------------------------//
   .start({ port: PORT })
