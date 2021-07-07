b89f16cd46def1bd35e41c99e32f18551ae76b2d
Cengizhan Ziyaeddin <cengiz.ziyaeddin@sigmalabs.xyz>
2021-07-07 12:04:38 +0100

    seedRecipeData.js now formats JSON

--- backend/seedRecipeData.js
+++ backend/seedRecipeData.js
@@ -1,5 +1,5 @@
 
-const getSeedData = () => {
+const getSeedData = async () => {
     const endpoint = `https://raw.githubusercontent.com/tabatkins/recipe-db/master/db-recipes.json`
 
     const data = await fetch(endpoint)
@@ -9,4 +9,25 @@ const getSeedData = () => {
     return recipeData
 }
 
-//Todo: Crea
+//Todo: name instructions, ingredients, tags
+
+
+const formatData = async () => {
+
+    const recipeData = await getSeedData()
+
+    const recipesArray = []
+    
+    for(let recipeId in recipeData){
+        const recipe = {
+            name: recipeData[recipeId].name,
+            ingredients: recipeData[recipeId].ingredients,
+            instructions: recipeData[recipeId].instructions,
+            tage: recipeData[recipeId].tags
+        }
+        recipesArray.push(recipe)
+    }
+    return recipesArray
+}
+
+formatData()
