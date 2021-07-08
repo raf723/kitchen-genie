c3d300620ac765fbc3e74fa68358342241293a12
Cengizhan Ziyaeddin <cengiz.ziyaeddin@sigmalabs.xyz>
2021-07-08 19:07:11 +0100

    SVG attributes renamed to camelcase

--- frontend/src/components/navbar.js
+++ frontend/src/components/navbar.js
@@ -6,7 +6,7 @@ function Navbar( {userAuthenticated} ) {
   return(
     <div>
       <nav className="page-nav">
-        <NavLink className="logo-link" activeClassName="active" exact to='/'><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="rgb(116,166,127)" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></NavLink>
+        <NavLink className="logo-link" activeClassName="active" exact to='/'><svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="rgb(116,166,127)" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></NavLink>
         <div className="middle-nav">
           {userAuthenticated === false && <NavLink className="nav-link" activeClassName="active" to='/login'>Login</NavLink>}
           {userAuthenticated === false && <NavLink className="nav-link" activeClassName="active" to='/register'>Create Account</NavLink>}
