59b69567e0d6e83266562b5f76ebdef87b8af9fc
Raf <rafansari97@gmail.com>
2021-07-08 13:22:01 +0100

    App.js route patch

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -10,8 +10,7 @@ import Home from './home'
 import Register from './register'
 import Login from './login'
 import Results from './results'
-import Home from './home'
-import About from './About-us'
+import About from './about'
 
 class App extends React.Component {
   constructor() {
@@ -23,28 +22,40 @@ class App extends React.Component {
     return(
       <Router>
         <div>
+          {/* Pass authentication result as a prop to toggle navigation bar buttons */}
           <Navbar userAuthenticated={ this.state.userAuthenticated }/>
 
           <Switch>
-            <Route path='/'>
+
+            {/* Homepage */}
+            <Route exact path='/'>
               <Home />
             </Route>
 
+            {/* Register */}
             <Route path='/register'>
               <Register />
             </Route>
 
+            {/* Login page */}
             <Route path='/login'>
               <Login />
             </Route>
-      
-            <Route path='/about-us'>
+
+            {/* About us */}
+            <Route path='/about'>
               <About />
             </Route>
 
+            {/* Saved recipes */}
             <Route path='/favourites'>
               <h1>Favourites</h1>
             </Route>
+
+            {/* Recipe results */}
+            <Route path='/results' component={ Results }>
+            </Route>
+
           </Switch>
         </div>
       </Router>
