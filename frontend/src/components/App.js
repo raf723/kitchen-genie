74a0030fc4cf727af8e9f5e00690c4b4e489cd79
Keano Robinson <keano@Keanos-MBP.home>
2021-07-10 13:07:30 +0100

    Bug fix: restore accedentally deleted method

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -54,9 +54,27 @@ class App extends React.Component {
     window.location.replace('/')
   }
 
+  handleSaveRecipe = async (recipeId, toSave=true) => {
+    const apiResponse = await fetch(`http://localhost:8080/save/${recipeId}/${ toSave ? 'save' : 'unsave'}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    const { response } = await apiResponse.json()
+
+    if (response === 'success') {
+      return toSave
+    } else if (response === 'bad credential') {
+        alert("Unauthorized access!\nYou must log in to access saved recipes!")
+        window.location.replace('/login')
+    } else {
+        window.location.replace('/error')
+    }
+  }
 
   render() {
-    const {loggedInUser } = this.state
+    const { loggedInUser } = this.state
     return(
       <Router>
         {/* Pass authentication result as a prop to toggle navigation bar buttons */}
@@ -92,7 +110,7 @@ class App extends React.Component {
 
             {/* Saved recipes */}
             <Route path='/favourites'>
-              <SavedRecipes />
+              <SavedRecipes onSaveRecipe={this.handleSaveRecipe}/>
             </Route>
 
             {/* Recipe results */}
