25c5a67453e3feb0b893564c99534bcb80c6dfcc
Keano Robinson <keano@Keanos-MBP.home>
2021-07-10 12:22:33 +0100

    Update SaveButtton component and SavedRecipes component

--- frontend/src/components/SavedRecipes.js
+++ frontend/src/components/SavedRecipes.js
@@ -1,39 +1,68 @@
 import { Component } from 'react'
-import SaveButton from './SaveButton'
 import RecipeCard from './RecipeCard'
+import SaveButton from './SaveButton'
+import '../css/saved-recipes.css'
 
 export class SavedRecipes extends Component {
     initialState = {
+        loggedInUser: {},
         savedRecipes: [],
         isCurrentlySaved: {}
     }
-    componentDidMount(){
-        //fetch saved recipes from database and add them to state
-        //render recipe cards of saved recipes
+
+    state = this.initialState
+
+    async componentDidMount() {
+        const apiResponse = await fetch(`http://localhost:8080/myrecipes`, {
+            credentials: 'include',
+            headers: { 'Content-Type': 'application/json' },
+        })
+        const { response, recipes, loggedInUser } = await apiResponse.json()
+        if (response === 'success') { 
+            const isCurrentlySaved =  {}
+            recipes.forEach((recipe) => isCurrentlySaved[recipe.id] = true)
+            this.setState({savedRecipes: recipes, isCurrentlySaved, loggedInUser})
+        } else if (response === 'unauthorized') {
+            //do nothing
+        } else if (response === 'service down') { 
+            //notify the user (e.g. when access limit reached )
+        }else {
+            window.location.replace('/error')
+        }
     }
 
-    handleSave(recipeId) {
-        //make a POST to bakend with sessionId credentials to 
-        //Vote status 
+    async handleSaveRecipe(recipeId) {
+        const { isCurrentlySaved } = this.state
+        const newSaveState = await this.props.onSaveRecipe(recipeId, !isCurrentlySaved[recipeId])
+        isCurrentlySaved[recipeId] = newSaveState
+        this.setState({ isCurrentlySaved })
     }
 
     renderCard(recipe){
+        const { isCurrentlySaved } = this.state
         return (
-            <RecipeCard id={recipe.id} 
-                        image={recipe.image} 
-                        numIngredients={recipe.numIngredients} 
-                        numMissingIngredients={recipe.numMissingIngredients}
-                        onSave={() => this.handleSave(recipe.id)}
-                        isCurrentlySaved={this.state.isCurrentlySaved[recipe.id]} />
+            <div key={recipe.id} className="favourite-recipe">
+                { 
+                    <SaveButton onSave={ async () => await this.handleSaveRecipe(recipe.id)} 
+                    isCurrentlySaved={isCurrentlySaved[recipe.id]}/>
+                }
+                <RecipeCard  
+                            id={recipe.id} 
+                            title={recipe.title}
+                            image={recipe.image} 
+                            numIngredients={recipe.numIngredients} 
+                            numMissingIngredients={recipe.numMissingIngredients} />
+            </div>
         )
     }
 
     render() {
+        const { savedRecipes, loggedInUser } = this.state
         return (
-            <div className="page-container">
-                <h1> Saved Recipes </h1>
+            <div className="saved-recipes-page-container">
+                <h1> Hi {loggedInUser.username}! <br/> Servrnig Up Your Faves 🍽</h1>
                 <div className="card-display-container">
-                    {/* For each recipe in saved recipes render a card*/}
+                    { savedRecipes.map((recipe) => this.renderCard(recipe)) }
                 </div>
             </div>
         )
