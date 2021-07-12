1be92058c65fc0f845a1eb55c2bedd6db9e7d437
Raf <rafansari97@gmail.com>
2021-07-12 11:22:14 +0100

    Remove 'app.js'

--- frontend/src/components/app.js
+++ frontend/src/components/app.js
@@ -13,6 +13,9 @@ import Login from './Login'
 import Results from './Results'
 import About from './About'
 import Recipe from './Recipe'
+import SavedRecipes from './SavedRecipes'
+
+
 
 class App extends React.Component {
   initialState = {
@@ -49,8 +52,27 @@ class App extends React.Component {
   }
   
   handleLogout() {
-    setCookie('sessionID', null, 0)
-    this.setState({...this.initialState})
+    setCookie('sessionId', null, 0)
+    window.location.replace('/')
+  }
+
+  handleSaveRecipe = async (recipeId, toSave=true) => {
+    const apiResponse = await fetch(`http://localhost:8080/save/${recipeId}/${ toSave ? 'save' : 'unsave'}`, {
+      method: 'POST',
+      credentials: 'include',
+      headers: { 'Content-Type': 'application/json' },
+    })
+
+    const { response } = await apiResponse.json()
+
+    if (response === 'success') {
+      return toSave
+    } else if (response === 'bad credential') {
+        alert("Unauthorized access!\nYou must log in to access saved recipes!")
+        window.location.replace('/login')
+    } else {
+        window.location.replace('/error')
+    }
   }
 
   render() {
@@ -89,7 +111,7 @@ class App extends React.Component {
 
             {/* Saved recipes */}
             <Route path='/favourites'>
-              <h1>Favourites</h1>
+              <SavedRecipes onSaveRecipe={this.handleSaveRecipe}/>
             </Route>
 
             {/* Recipe results */}
