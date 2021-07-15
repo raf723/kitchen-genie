3e8728a934b5506e082ed33d26ecd10ad64208a6
tawabrahmani <tawabrahmani@hotmail.co.uk>
2021-07-15 11:47:29 +0100

    Error Page altered and comments sections styling complete

--- frontend/src/components/CommentInput.js
+++ frontend/src/components/CommentInput.js
@@ -30,23 +30,46 @@ class InputComments extends React.Component {
         }
     }
 
+    autoGrow(element) {
+        element.target.style.height = "5px";
+        element.target.style.height = (element.target.scrollHeight)+"px";
+    }
+
     renderCommentBox(userAuthenticated){
         const { comment } = this.state
+
+
+
         if (!!userAuthenticated) {
             return (
                 <div className="comment-input-container">
-                    <h3 className="user-prompt">Hi, {userAuthenticated.username}! Leave a comment here!</h3>
+                    <h3>Hi, <span className="user-prompt">{userAuthenticated.username}!</span> Leave a comment below!</h3>
                     <form onSubmit={(e) => this.handleSubmit(e)}>
-                    <textarea name="comment" placeholder="Please enter a comment..." value={comment} className="comment-box" onChange={(e) => this.setState({comment: e.target.value})}/>
-                    <div className="comment-buttons">
-                        <button type="submit" name="post" value="Post">Submit</button>
-                        <button type="button" value="Cancel" name="cancel" onClick={() => this.setState({ comment: "" })}>Cancel</button>
-                    </div>
+                        <div className="comment-and-buttons">
+                            <textarea name="comment" placeholder="Please enter a comment..." value={comment} className="comment-box" onInput={this.autoGrow} onChange={(e) => this.setState({comment: e.target.value})}/>
+                            <div>
+                                <button className="cancel-button" type="button" value="Cancel" name="cancel" onClick={() => this.setState({ comment: "" })}>Cancel</button>
+                                <button className="post-button" type="submit" name="post" value="Post">Post</button>
+                            </div>                        
+                        </div>
                     </form>
                 </div>
             )
         } else {
-            return <h3 className="user-prompt">Log in <Link className="login-link" to="/login">here</Link> to add a comment!</h3>
+            return (
+                <div className="comment-input-container">
+                    <h3>Please leave a comment below!</h3>
+                        <div className="comment-and-buttons">
+                            <textarea className="comment-box3" value={comment} name="comment" placeholder="You must be logged in to leave a comment." disabled/>
+                            <div>
+                                <button className="sign-up-button" type="button" name="sign-up-butto" onClick={() =>  window.location.href='/register'}>Sign Up</button>
+                                <button className="log-in-button" type="button" name="log-in-button" onClick={() =>  window.location.href='/login'}>Log In</button>
+                            </div>                        
+                        </div>
+                </div>
+                // {/* <button onclick="location.href = 'www.yoursite.com';" id="myButton" class="float-left submit-button" >Home</button> */}
+            // {/* <h3 className="user-prompt">Log in <Link className="login-link" to="/login">here</Link> to add a comment!</h3> */}
+            )
         }
     }
 
